bcd4fcc3994b567aca3330ecd82b93fa8e7a2312
Yanyan Hu <yanyanhu@sg.ibm.com>
2017-06-17 04:56:07 +0000

    Add comments to scripts/start-fabric.sh

--- deploy/scripts/start-fabric.sh
+++ deploy/scripts/start-fabric.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 
 
+echo "====Start Fabric Network===="
+
+# Create couchdb and offchaindb
+echo "kubectl create -f fabric-couchdb.yaml"
 kubectl create -f fabric-couchdb.yaml
+echo "kubectl create -f fabric-offchaindb.yaml"
 kubectl create -f fabric-offchaindb.yaml
 
-sleep 5
-
+# Create ca
+echo "kubectl create -f fabric-ca-org1.yaml"
 kubectl create -f fabric-ca-org1.yaml
+echo "kubectl create -f fabric-ca-org2.yaml"
 kubectl create -f fabric-ca-org2.yaml
 
-sleep 3
-
+# Create orderer
+echo "kubectl create -f fabric-orderer.yaml"
 kubectl create -f fabric-orderer.yaml
 
-sleep 3
+# TODO(YanyanHu):find better way to fix the dependency
+# between couchdb and peer.
+sleep 5
 
+# Create peers
+echo "kubectl create -f fabric-peer0-org1.yaml"
 kubectl create -f fabric-peer0-org1.yaml
+echo "kubectl create -f fabric-peer1-org1.yaml"
 kubectl create -f fabric-peer1-org1.yaml
+echo "kubectl create -f fabric-peer0-org2.yaml"
 kubectl create -f fabric-peer0-org2.yaml
+echo "kubectl create -f fabric-peer1-org2.yaml"
 kubectl create -f fabric-peer1-org2.yaml
 
-sleep 3
-
+# Create test env
+echo "kubectl create -f fabric-test.yaml"
 kubectl create -f fabric-test.yaml
 
 echo "====done===="
